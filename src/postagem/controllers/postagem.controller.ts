199b1ed6994034fd70cda24a2617b6156bd4bc53
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-09-23 14:42:35 -0300

    Primeira parte do projeto

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, HttpCode, HttpStatus} from '@nestjs/common';
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.service';
+
+@Controller('/postagens')
+export class PostagemController {
+  constructor(private readonly postagemSevice: PostagemService){}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Postagem[]> {
+    return this.postagemSevice.findAll();
+  }
+}
