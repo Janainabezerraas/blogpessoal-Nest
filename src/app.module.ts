19e87f73738577a85042b69c08ca4c868a50c194
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-09-30 15:51:45 -0300

    modulo auth e modulo usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+import { UsuarioModule } from './usuário/usuario.module';
+import { Usuario } from './usuário/entities/usuario.entity';
 
 @Module({
   imports: [
@@ -14,12 +17,14 @@ import { TemaModule } from './tema/tema.module';
       username: 'root',
       password: '*@5.!6uVbC07ml$',
       database: 'db_blogpessoal',
-      entities: [Postagem, Tema],
+      entities: [Postagem, Tema, Usuario],
       synchronize: true,
       logging: true,
     }),
     PostagemModule,
     TemaModule,
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [],
   providers: [],
