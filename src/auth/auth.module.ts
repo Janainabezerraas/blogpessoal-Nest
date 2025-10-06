5f35ce3d4480f72601bdc4ccf1edde0e0ca26a35
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-10-06 11:16:49 -0300

    Modulo Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,25 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { Bcrypt } from "./bcrypt/bcrypt";
+import { UsuarioModule } from "../usuário/usuario.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants/constants";
+import { AuthService } from "./services/auth.service";
+import { LocalStategy } from "./strategy/local.strategy";
+import { AuthController } from "./controllers/auth.controller";
+import { JwtStrategy } from "./strategy/jwt.strategy";
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
-  controllers: [],
+  imports: [
+    forwardRef(() => UsuarioModule),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "365d"},
+    })
+  ],
+  providers: [Bcrypt, AuthService, LocalStategy, JwtStrategy],
+  controllers: [AuthController],
   exports: [Bcrypt],
 })
 export class AuthModule {};
---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'cP6ZCXL6t8CvTp4cXv51TbelgCi6PFCTTl/z72zDat7kwK9oNEaB80GuCzohy6Ur',
+};
